27ce710248e6dd50afd6da9aeaf88b71abd2276e
John Connor <hrgplumer@gmail.com>
2016-08-28 14:16:42 -0400

    add empty mvc project

---
+++ On3SpiderMVC/On3SpiderMVC/Startup.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(On3SpiderMVC.Startup))]
+namespace On3SpiderMVC
+{
+    public partial class Startup
+    {
+
+    }
+}
